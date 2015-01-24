9845142b893a1e942db22a499eecea8e0ab01fe7
Eric <bookjunkie93@gmail.com>
2015-01-24 04:44:30 -0500

    updated PlayerManager.cs

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,7 +2,12 @@
 using System.Collections;
 
 public class PlayerManager : MonoBehaviour {
-
+	public float health;
+	public float hunger;
+	public float insanity;
+	public float oxygen;
+	public float timeElapsed;
+	public float score;
 	// Use this for initialization
 	void Start () {
 	
